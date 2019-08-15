30932ec12686991b6420590b18dc4b4288839ee0
Junaid Ahsan <mahsan@microsoft.com>
2019-08-15 11:41:48 -0400

    Module 4: first checkin

--- src/app/product-description/product-description.component.ts
+++ src/app/product-description/product-description.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ProductService } from '../product.service';
+import { Album } from 'app/album';
 
 
 @Component({
@@ -8,13 +9,12 @@ import { ProductService } from '../product.service';
   styleUrls: ['./product-description.component.css']
 })
 export class ProductDescriptionComponent implements OnInit {
-  albumInfo;
+  albumInfo: Album;
 
   constructor(private _productService: ProductService) { }
 
   ngOnInit() {
     this._productService.getAlbum(1).subscribe(response => this.albumInfo = response);
-    console.log(this.albumInfo);
   }
 
 }
--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -1,6 +1,9 @@
 import { Injectable } from '@angular/core';
 import { Http, Response } from '@angular/http';
+import { Album } from './album';
+import { Observable } from 'rxjs/Observable';
 import 'rxjs/add/operator/map';
+
 
 
 @Injectable()
@@ -9,7 +12,7 @@ private _albumUrl = '../assets/album.json';
 
   constructor(private _http: Http) { }
 
-  getAlbum(id:number){
-    return this._http.get(this._albumUrl).map((response) => response.json());
+  getAlbum(id:number):Observable<Album>{
+    return this._http.get(this._albumUrl).map((response) => <Album>response.json());
   }
 }
